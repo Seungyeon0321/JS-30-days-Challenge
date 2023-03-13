332a2cc98e562db7673fe335cb975b8eff994b09
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2023-03-13 08:45:57 -0700

    second colorpicker

--- 11 - color picker/index.js
+++ 11 - color picker/index.js
@@ -8,10 +8,50 @@ class ColorPicker {
         this.savedColors = this.getSavedColors();
 
         this.colorjoe.show();
+        this.setSelectedColor("#009578");
+
+        this.colorjoe.on("change", color => {
+            this.setSelectedColor(color.hex(), true)
+        })
+
+        this.root.querySelectorAll(".saved-color").forEach((el, i) => {
+            this.showSavedColor(el, this.savedColors[i]);
+
+            el.addEventListener("click", e => {
+
+                console.log(this.showSavedColor);
+                if (e.button == 1) {
+                    this.saveColors(this.selectedColor, i);
+                    this.showSavedColor(el, this.selectedColor);
+                }
+
+                this.setSelectedColor(el.dataset.color);
+            });
+        })
+    }
+
+    setSelectedColor(color, skipCjUpdate = false) {
+        this.selectedColor = color;
+        this.root.querySelector('.selected-color-text').textContent = color;
+        this.root.querySelector('.selected-color').style.background = color;
+
+        if (!skipCjUpdate) {
+            this.colorjoe.set(color);
+        }
     }
 
     getSavedColors() {
         return ["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"];
+    }
+
+    showSavedColor(element, color) {
+        element.style.background = color;
+        element.dataset.color = color;
+    }
+
+    saveColor(color, i) {
+        // this.savedColors[i] = color;
+        // localStorage.setItem("colorPicked-saved", JSON.stringify(this.savedColors));
     }
 
 }
